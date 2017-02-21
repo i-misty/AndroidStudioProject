c605196e5ee01db9b599df365293a4f672aec847
zhangwu <az460024285@qq.com>
2017-02-21 11:33:19 +0800

    test

--- bigfilecopyinstance/src/main/java/com/carousel/bigfilecopyinstance/MainActivity.java
+++ bigfilecopyinstance/src/main/java/com/carousel/bigfilecopyinstance/MainActivity.java
@@ -9,5 +9,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    public  void readFile(){
+        try {
+            File file = new File("test.txt"); //如果文件不存在，则自动创建
+            BufferedSink sink = Okio.buffer(Okio.sink(file));
+            sink.writeUtf8("Hello, World");
+            sink.writeString("测试信息", Charset.forName("UTF-8"));
+            sink.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
